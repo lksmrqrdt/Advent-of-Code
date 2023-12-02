48e8aaf3abc66c148f7c162b8cefec7234e50c5e
Lukas Marquardt <git@lksmrqrdt.com>
2023-12-02 15:05:06 +0100

    feat: solve day 2 part 1 This was surprisingly troublesome, as I haven't used Regex in a while. It left me wondering, why I wasn't able to just get every subgroup at once. My longest Regex attempt while debugging looked like this: \Game (?<gameID>\d+): (?<rounds>(?:(?<round>(?:(?<amount>\d+) (?<color>\w+),? ?)+);? ?)+)\g

---
+++ 2023/solutions/typescript/day-2/part-1.ts
@@ -0,0 +1,64 @@
+import AOC from "../aoc";
+
+export default class Part1 extends AOC {
+	constructor() {
+		super(2);
+	}
+
+  private maxValid = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  }
+  
+  private regex = {
+    game: /Game (?<game>\d+): (?<rounds>.*)/g,
+    rounds: /(?:\d+ \w+,? ?)+;?/g,
+    score: /(?<amount>\d+) (?<color>\w+)/g
+  }
+
+	private validGamesSum = 0;
+
+	async solve() {
+		super
+			.readInput()
+			.then((input) => {
+				const games = input.matchAll(this.regex.game);
+				for (const round of games) {
+					const game = round.groups!.game;
+					const rounds = round.groups!.rounds;
+
+					if (this.validateRounds(rounds)) {
+						this.validGamesSum += Number.parseInt(game);
+					}
+				}
+			})
+			.finally(() => {
+				console.log(this.validGamesSum);
+			});
+	}
+
+	private validateRounds(rounds: string): boolean {
+		const roundArray = rounds.matchAll(this.regex.rounds);
+
+		for (const round of roundArray) {
+			const score = round[0].matchAll(this.regex.score);
+
+			for (const s of score) {
+				if (
+					(s.groups?.color === "red" &&
+						Number(s.groups.amount) > this.maxValid.red) ||
+					(s.groups?.color === "green" &&
+						Number(s.groups.amount) > this.maxValid.green) ||
+					(s.groups?.color === "blue" &&
+						Number(s.groups.amount) > this.maxValid.blue)
+				) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
+
+new Part1().solve();
