a7186b46551b1dd7902ae92ee903b8b129090526
Lukas Marquardt <git@lksmrqrdt.com>
2023-12-04 11:15:10 +0100

    feat: solve day 4 part 1 Not exactly the cleanest code, but I'll fix it later. As it turns out, my colleagues are quite fast, so I had no time to make great code :)

---
+++ 2023/solutions/typescript/day-4/part-1.ts
@@ -0,0 +1,34 @@
+import AOC from "../aoc";
+
+export default class Part1 extends AOC {
+  constructor() {
+    super(4);
+
+    this.lines = this.input.split("\n");
+  }
+
+  private readonly lines: string[];
+
+  async solve(): Promise<number> {
+    let value = 0;
+    for (const line of this.lines) {
+      const cardContent = line.split(":")[1].trim().split("|");
+      // Some cards have multiple white spaces between words, so we need to trim them
+      // using a regex
+      const winning = new Set(cardContent[0].trim().split(/\s+/));
+      const guessed = new Set(cardContent[1].trim().split(/\s+/));
+
+      let matches = 0;
+      for (const item of guessed) {
+        if (winning.has(item)) {
+          matches += 1;
+        }
+      }
+
+      if (matches > 0) {
+        value += 2 ** (matches - 1);
+      }
+    }
+    return value;
+  }
+}
--- 2023/solutions/typescript/index.ts
+++ 2023/solutions/typescript/index.ts
@@ -4,6 +4,7 @@ import D2P1 from "./day-2/part-1";
 import D2P2 from "./day-2/part-2";
 import D3P1 from "./day-3/part-1";
 import D3P2 from "./day-3/part-2";
+import D4P1 from "./day-4/part-1";
 
 export default async function index() {
 	console.log(`Day 1 - Part 1: ${await new D1P1().solve()}`);
@@ -12,6 +13,7 @@ export default async function index() {
 	console.log(`Day 2 - Part 2: ${await new D2P2().solve()}`);
 	console.log(`Day 3 - Part 1: ${await new D3P1().solve()}`);
 	console.log(`Day 3 - Part 2: ${await new D3P2().solve()}`);
+	console.log(`Day 4 - Part 1: ${await new D4P1().solve()}`);
 }
 
 await index();
